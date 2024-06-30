f3fc329cea705d467aeea46f13b5d3559399a663
Patrick Kenekayoro <ktarila@gmail.com>
2024-06-30 20:47:19 +0100

    enable neo-tree

--- init.lua
+++ init.lua
@@ -989,8 +989,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   {
     'akinsho/bufferline.nvim',
