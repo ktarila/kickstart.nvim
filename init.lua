4e5cbea3e92d8538bf8d5ec3c74265c1bbde9373
Patrick Kenekayoro <ktarila@gmail.com>
2024-07-13 16:44:01 +0100

    add neoterm and shortcut to run a single command

--- init.lua
+++ init.lua
@@ -572,7 +572,7 @@ require('lazy').setup({
 
           null_ls.builtins.formatting.prettier.with {
             command = 'prettier',
-            args = { '--stdin-filepath', '$FILENAME', '--parser', 'html' },
+            args = { '--stdin-filepath', '$FILENAME' },
             to_stdin = true,
             filetypes = { 'twig' },
           },
@@ -757,7 +757,7 @@ require('lazy').setup({
       formatters = {
         prettier_twig = {
           command = 'prettier',
-          args = { '--stdin-filepath', '$FILENAME', '--parser', 'html' },
+          args = { '--stdin-filepath', '$FILENAME' },
           tempfile_postfix = '.tmp',
         },
         -- Add other formatters as needed
@@ -1100,6 +1100,80 @@ vim.api.nvim_set_keymap('n', '<C-w><Up>', ':resize -2<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-w><Down>', ':resize +2<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-w><Left>', ':vertical resize -2<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-w><Right>', ':vertical resize +2<CR>', opts)
+
+vim.g.neoterm_size = tostring(0.3 * vim.o.columns)
+vim.g.neoterm_default_mod = 'botright vertical'
+
+vim.api.nvim_set_keymap('n', '<leader>.e', ':TaskThenExit ', {})
+vim.api.nvim_set_keymap('n', '<leader>.p', ':TaskPersist ', {})
+vim.api.nvim_set_keymap('n', '<leader>.t', ':1Ttoggle<CR><ESC>', {})
+vim.api.nvim_set_keymap('n', '<leader>..', ':TaskPersist<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>.s', ':SetTaskCommand<CR>', {})
+
+local Input = require 'nui.input'
+local event = require('nui.utils.autocmd').event
+
+local stored_task_command = nil
+
+local trigger_set_command_input = function(callback_fn)
+  local input_component = Input({
+    position = '50%',
+    size = {
+      width = 50,
+    },
+    border = {
+      style = 'single',
+      text = {
+        top = 'Commmand to run:',
+        top_align = 'center',
+      },
+    },
+    win_options = {
+      winhighlight = 'Normal:Normal,FloatBorder:Normal',
+    },
+  }, {
+    prompt = '> ',
+    default_value = '',
+    on_submit = function(value)
+      stored_task_command = value
+      callback_fn()
+    end,
+  })
+
+  input_component:mount()
+  input_component:on(event.BufLeave, function()
+    input_component:unmount()
+  end)
+end
+
+vim.api.nvim_create_user_command('SetTaskCommand', function()
+  trigger_set_command_input(function()
+    -- Don't need to do anything here beyond set it
+  end)
+end, {})
+
+vim.api.nvim_create_user_command('TaskThenExit', function(input)
+  local cmd = input.args
+  vim.api.nvim_command ':Tnew'
+  vim.api.nvim_command(':T ' .. cmd .. ' && exit')
+end, { bang = true, nargs = '*' })
+
+vim.api.nvim_create_user_command('TaskPersist', function(input)
+  local execute = function(cmd)
+    vim.api.nvim_command ':1Tclear'
+    vim.api.nvim_command(':1T ' .. cmd .. ' && exit')
+  end
+
+  local one_off_command = input.args
+
+  if one_off_command and string.len(one_off_command) > 0 then
+    execute(one_off_command)
+  else
+    trigger_set_command_input(function()
+      execute(stored_task_command)
+    end)
+  end
+end, { nargs = '*' })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
