dd928fb6f1233d096f1267a705f36c55b46d85bf
Patrick Kenekayoro <ktarila@gmail.com>
2024-09-08 17:03:45 +0100

    formart after save - fix timeout issue

--- init.lua
+++ init.lua
@@ -803,7 +803,7 @@ require('lazy').setup({
         },
         -- Add other formatters as needed
       },
-      format_on_save = function(bufnr)
+      format_after_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
